6a8cbc61f71d8702902a2a96ace4b0bff0573ba8
Vaishali Tyagi <151373272+vaishalityagi633@users.noreply.github.com>
2025-01-22 14:44:26 +0530

    Update program_2.py

--- folder_2/program_2.py
+++ folder_2/program_2.py
@@ -1,5 +1,5 @@
 def main():
-    print("This is program 2 for detecting change in the folder_2.")
+    print("This is program 2 for detecting changes in the folder_2.")
 
 if __name__ == "__main__":
     main()
